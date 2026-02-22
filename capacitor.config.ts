170ca88843dcfc7cf42b0a98537648f195346817
Vítor Matias <vitor.matias@pm.me>
2026-02-22 16:10:07 +0000

    feat: Add Capacitor asset generation, including a new icon, and configure Capacitor server URL.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/core';
+import type { CapacitorConfig } from "@capacitor/core"
 
 const config: CapacitorConfig = {
-  appId: 'org.capuchinhos.biblia',
-  appName: 'Bíblia dos Capuchinhos',
-  webDir: 'dist/bible-app/browser',
-};
+  appId: "org.capuchinhos.biblia",
+  appName: "Bíblia dos Capuchinhos",
+  webDir: "dist/bible-app/browser",
+  server: {
+    url: "https://biblia.capuchinhos.org",
+    cleartext: true,
+  },
+}
 
-export default config;
+export default config
